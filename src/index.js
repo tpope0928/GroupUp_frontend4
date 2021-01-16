06b3ce14d724c66422f5d20f81cac11b8b1b0b59
tpope0928 <tpope0928@gmail.com>
2021-01-16 18:07:19 -0500

    index.js work

--- src/index.js
+++ src/index.js
@@ -4,3 +4,40 @@ const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
 
 let playersContainer = document.querySelector('div.player-container')
 let gamesContainer = document.querySelector('div.games-container')
+let playerWrapper = document.createElement("div")
+let gameWrapper = document.createElement("div")
+playerWrapper.id = 'player-wrapper'
+gameWrapper.id = 'game-wrapper'
+
+let homeButton = document.querySelector('#home')
+    homeButton.addEventListener("click", function () {
+        gamesContainer.textContent = ''
+        loadPage()
+    })
+
+function loadPage() {
+    playersContainer.textContent = ''
+    gamesContainer.textContent = ''
+    playerWrapper.textContent = ''
+    gameWrapper.textContent = ''
+
+    createPlayerForm();
+    fetchPlayers();
+
+    playersContainer.appendChild((createPlayerForm());
+
+    document.querySelector('#create-player').addEventListener("submit", function (e){
+        e.preventDefault();
+
+        let playerInfo = {
+            e.target.elements.name.value = '';
+            e.target.elements.city.value = '';
+            e.target.elements.state.value = '';
+
+            savePlayer(playerInfo)
+        })
+    })
+}
+
+loadPage()
+
