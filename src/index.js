e7776997cc704ab73a694bd9aae359eeabe73ea7
tpope0928 <tpope0928@gmail.com>
2021-01-16 17:52:51 -0500

    playerFunction fixed

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const BACKEND_URL = "http://localhost:3000";
+const PLAYERS_URL = `${BACKEND_URL}/api/v1/players`;
+const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
+
+let playersContainer = document.querySelector('div.player-container')
+let gamesContainer = document.querySelector('div.games-container')
