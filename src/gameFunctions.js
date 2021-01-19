a83a64393a8aa732f0536837ebd6836edaf71933
tpope0928 <tpope0928@gmail.com>
2021-01-18 20:13:19 -0500

    game functions page

---
+++ src/gameFunctions.js
@@ -0,0 +1,52 @@
+function saveGame(gameData) {
+    let game = {
+        player_id: gameData.player_id,
+        title: gameData.title,
+        genre: gameData.genre,
+        skill_level: gameData.skill_level,
+        game_name: gameData.game_name
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(game)
+    };
+
+    return fetch(GAMES_URL, configObj)
+        .then(res => res.json())
+        .then(obj => {
+            console.log(obj)
+    })
+}
+
+function renderGames(gamesArray, player_id) {
+    gamesArray.forEach(game => {
+        let newGame = new Game(game)
+    })
+}
+
+function createGames(player, gamesArray) {
+    gamesArray.forEach(game => {
+        let newGame = new Game(game)
+        player.games.push(newGame)
+    })
+}
+
+function fetchGames(player_id) {
+    return fetchGames(GAMES_URL)
+        .then(res => res.json())
+        .then(game => {
+            renderGames(game.data, player_id);
+        })
+}
+
+function listGames(player) {
+    gameWrapper.textContent = '';
+    player.games.forEach(game => {
+        addGame(Game)
+    })
+}
